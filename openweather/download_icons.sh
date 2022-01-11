8d8478041d475e3fd6c1e43a3ddb5df8698ef1c6
Michal Krissak <michal.krissak@vacuumlabs.com>
2022-01-11 22:28:13 +0100

    MOD icond names

--- openweather/download_icons.sh
+++ openweather/download_icons.sh
@@ -3,4 +3,5 @@
 cat ./icons.txt | while read icon out; do
   wget http://openweathermap.org/img/wn/${icon}.png -O 1x9${out}.png
   wget http://openweathermap.org/img/wn/${icon}@2x.png -O 2x9${out}.png
+  wget http://openweathermap.org/img/wn/${icon}@2x.png -O 4x9${out}.png
 done
